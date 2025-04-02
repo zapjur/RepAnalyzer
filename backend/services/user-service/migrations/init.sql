430cd12aa3e8f1f162d5696842339ff94feec6d7
Piotr Zapiór <piotrzapior0@gmail.com>
2025-04-02 17:45:19 +0200

    Added: viedos table to user_db

--- backend/services/user-service/migrations/init.sql
+++ backend/services/user-service/migrations/init.sql
@@ -1,8 +1,18 @@
 CREATE TABLE IF NOT EXISTS users (
-                                     id SERIAL PRIMARY KEY,
-                                     auth0_id TEXT UNIQUE NOT NULL,
-                                     email TEXT NOT NULL,
-                                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id SERIAL PRIMARY KEY,
+    auth0_id TEXT UNIQUE NOT NULL,
+    email TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE INDEX IF NOT EXISTS idx_users_auth0_id ON users(auth0_id);
+
+CREATE TABLE IF NOT EXISTS videos (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    url TEXT NOT NULL,
+    exercise_name TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+CREATE INDEX IF NOT EXISTS idx_videos_user_id ON videos(user_id);
